c3edbf495b717b4afc748affb0638136c8a3266e
StarshineC <141589458+StarshineC@users.noreply.github.com>
2026-05-17 23:48:54 -0400

    Projects.tsx | Tested a json import: Yeah it worked

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,4 +1,12 @@
 
+import data from './../assets/json/Projects.json';
+
+data.forEach(element => {
+    console.log(element);
+    console.log(element.name);
+    console.log(element.id);
+    console.log(element.description);
+});
 
 interface PropsProjects {
     id: string;
