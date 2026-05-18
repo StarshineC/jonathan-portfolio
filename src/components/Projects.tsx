3f1d3e7c1a288672c1367f9721de1757a3586479
StarshineC <141589458+StarshineC@users.noreply.github.com>
2026-05-18 00:53:27 -0400

    Projects.tsx | Added Sorting to the Projects List
    I may actually just make this an outside function.
    I think scraps will need it too.
    
    Wow sorting a json import is hard.
    I don't mean the sorting itself I mean I had looked a lot into
    how to get a mutable json import (I ended up just doing
    a structuredClone, there's not a lot).

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,12 +1,15 @@
 
 import data from './../assets/json/Projects.json';
 
-data.forEach(element => {
-    console.log(element);
-    console.log(element.name);
-    console.log(element.id);
-    console.log(element.description);
-});
+// Top 3 Projects
+
+
+
+// The Rest
+
+
+
+// Actual Projects Element
 
 interface PropsProjects {
     id: string;
@@ -15,6 +18,39 @@ interface PropsProjects {
 export default function Projects(
     { id }: PropsProjects
 ) {
+
+    let projects = structuredClone(data);
+
+    const sortProjects = (
+        sortBy: 'name' | 'date' | 'ai' = 'name',
+        reversed: boolean = false
+    ): void => {
+        switch (sortBy) {
+            case 'name':
+                projects.sort((a, b) => {
+                    if (reversed) return b.name.localeCompare(a.name);
+                    else return a.name.localeCompare(b.name);
+                });
+                break;
+            case 'date':
+                projects.sort((a, b) => {
+                    if (reversed) return b.timesort - a.timesort;
+                    else return a.timesort - b.timesort;
+                });
+                break;
+            case 'ai':
+                projects.sort((a, b) => {
+                    if (reversed) return b.aiuse - a.aiuse;
+                    else return a.aiuse - b.aiuse;
+                });
+                break;
+        }
+    };
+
+    sortProjects("ai", true);
+
+    console.log(projects);
+
     return (
         <div
             id={id}
