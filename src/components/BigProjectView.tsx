15ff4ca67c8c484201b31d7b1a8862ff6fca781d
StarshineC <141589458+StarshineC@users.noreply.github.com>
2026-05-19 15:19:51 -0400

    css/ | Changed most to modules.
    Apparently this is a thing vite can do, but it makes it
    so that css module imports only affect certain components.

--- src/components/BigProjectView.tsx
+++ src/components/BigProjectView.tsx
@@ -20,58 +20,63 @@ export default function BigProjectView(
     const [isLoading, setIsLoading] = useState(true);
     const [loadingError, setLoadingError] = useState(false);
 
+    const onClose = () => {
+        hideFunc();
+        setDetailsVisible(true);
+        setIsLoading(true);
+        setLoadingError(false);
+        console.log(`Details Visible: ${detailsVisible}`);
+    };
+
     return (
-        <div className = "big-project-view">
-            <button onClick={ () => {
-                hideFunc();
-                setDetailsVisible(true);
-                setIsLoading(true);
-                setLoadingError(false);
-                console.log(`Details Visible: ${detailsVisible}`);
-            }}
-            >
-                X
-            </button>
-            {
-                detailsVisible &&
-                <>
-                    <h2>{title}</h2>
-                    <p>{timelabel}</p>
-                    <p>{description}</p>
-                    <p>AI Use: {aiuse}</p>
-                    <p>{aisummary}</p>
-                </>
-            }
-            <p
-                onClick={ () => {
-                    setDetailsVisible(!detailsVisible);
-                }}
-            >
-                Image: {id}
-            </p>
-            {
-                isLoading &&
-                <p>Image Loading...</p>
-            }
-            {
-                loadingError &&
-                <p>Error loading image.</p>
-            }
-            {
-                !loadingError &&
-                <img
-                    src = {
-                        "/assets/img-projects/full-" + id + ".webp"
-                    }
-                    alt = {title}
-                    onLoad={() => setIsLoading(false)}
-                    onError={() => {
-                        setIsLoading(false);
-                        setLoadingError(true);
-                        console.log("Error loading image for project " + id);
+        <>
+            <div className = "big-project-view-back" onClick={onClose}></div>
+            <div className = "big-project-view">
+                <button onClick={ onClose }>
+                    X
+                </button>
+                {
+                    detailsVisible &&
+                    <>
+                        <h2>{title}</h2>
+                        <p>{timelabel}</p>
+                        <p>{description}</p>
+                        <p>AI Use: {aiuse}</p>
+                        <p>{aisummary}</p>
+                    </>
+                }
+                <p
+                    onClick={ () => {
+                        setDetailsVisible(!detailsVisible);
                     }}
-                />
-            }
-        </div>
+                    >
+                    Image: {id}
+                </p>
+                {
+                    isLoading &&
+                    <p>Image Loading...</p>
+                }
+                {
+                    loadingError &&
+                    <p>Error loading image.</p>
+                }
+                {
+                    !loadingError &&
+                    <img
+                        src = {
+                            "/assets/img-projects/full-" + id + ".webp"
+                        }
+                        alt = {title}
+                        onLoad={() => setIsLoading(false)}
+                        onError={() => {
+                            setIsLoading(false);
+                            setLoadingError(true);
+                            console.log("Error loading image for project " + id);
+                        }}
+                        onClick={ () => {setDetailsVisible(!detailsVisible)} }
+                    />
+                }
+            </div>
+        </>
     );
 }
