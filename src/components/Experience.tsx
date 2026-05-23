d1163dbc3c3c25071d5fc58c95912743938a5714
StarshineC <141589458+StarshineC@users.noreply.github.com>
2026-05-23 09:29:41 -0400

    Experience.tsx | Nesting was getting to large. I fixed that.
    I put experience entries and reference entries in their own functions.

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -3,84 +3,121 @@ import data from './../assets/json/Experience.json';
 import { removeLinkPrefix } from '../utility/stringEditor.ts';
 
 export default function Experience() {
-  return (
-    <section id="experience">
-        <div className="content-strip">
-            <h2>Experience</h2>
-            {
-                data.map( (entry) => (
-                    <div
-                        key={
-                            "experience-" + 
-                            entry.company + 
-                            "-" + 
-                            entry.role
-                        }
-                        className="experience-entry"
-                    >
-                        <h3>{entry.company}</h3>
-                        <p>{entry.location}</p>
-                        <p>{entry.role}</p>
-                        <p>{entry.time}</p>
-                        <p>{entry.description}</p>
-                        <div>
-                            {
-                                entry.references.map( (ref) => (
-                                    <div
-                                        key={
-                                            "experience-" +
-                                            entry.company + 
-                                            "-" + 
-                                            entry.role + 
-                                            "-" + 
-                                            ref.name
-                                        }
-                                    >
-                                        <p>{ref.name}</p>
-                                        <p>{ref.role}</p>
-                                        {
-                                            ref.contact.map( (contact) => (
-                                                <p
-                                                    key={
-                                                        "experience-" +
-                                                        entry.company + 
-                                                        "-" + 
-                                                        entry.role + 
-                                                        "-" + 
-                                                        ref.name + 
-                                                        "-" + 
-                                                        contact.info
-                                                    }
-                                                >
-                                                    {contact.method + ": "}
-                                                    <a 
-                                                        href= {
-                                                            contact.method === "Email" ? 
-                                                            ("mailto:" + contact.info) : 
-                                                            contact.method === "Phone" ?
-                                                            ("tel:" + contact.info) :
-                                                            contact.info
-                                                        }
-                                                        target="_blank"
-                                                        rel="noopener noreferrer"
-                                                    >
-                                                        {
-                                                            contact.method === "Phone" ? 
-                                                            contact.readable :
-                                                            removeLinkPrefix(contact.info)
-                                                        }
-                                                    </a>
-                                                </p>
-                                            ))
-                                        }
-                                    </div>
-                                ))
+
+    return (
+        <section id="experience">
+            <div className="content-strip">
+                <h2>Experience</h2>
+                {
+                    data.map( (entry) => (
+                        <ExperienceEntry
+                            key={
+                                "experience-" + 
+                                entry.company + 
+                                "-" + 
+                                entry.role
                             }
-                        </div>
-                    </div>
+                            entry = {entry} 
+                        />
+                    ))
+                }
+            </div>
+        </section>
+    );
+}
+
+interface PropsExperienceEntry {
+    entry: {
+        company: string;
+        location: string;
+        role: string;
+        time: string;
+        description: string;
+        references: {
+            name: string;
+            role: string;
+            contact: {
+                method: string;
+                info: string;
+                readable?: string;
+            }[];
+        }[];
+    };
+}
+
+function ExperienceEntry({ entry }: PropsExperienceEntry) {
+    return (
+        <div
+            className="experience-entry"
+        >
+            <h3>{entry.company}</h3>
+            <p>{entry.location}</p>
+            <p>{entry.role}</p>
+            <p>{entry.time}</p>
+            <p>{entry.description}</p>
+            <div>
+                {
+                    entry.references.map( (ref) => (
+                        <Reference
+                            key={
+                                "reference-" +
+                                ref.name
+                            }
+                            name={ref.name}
+                            role={ref.role}
+                            contacts={ref.contact}
+                        />
+                    ))
+                }
+            </div>
+        </div>
+    );
+}
+
+interface PropsReference {
+    name: string;
+    role: string;
+    contacts: {
+        method: string;
+        info: string;
+        readable?: string;
+    }[];
+}
+
+function Reference({ name, role, contacts }: PropsReference) {
+    return (
+        <div>
+            <p>{name}</p>
+            <p>{role}</p>
+            {
+                contacts.map( (contact) => (
+                    <p
+                        key={
+                            "contact-" +
+                            contact.info
+                        }
+                    >
+                        {contact.method + ": "}
+                        <a 
+                            href= {
+                                contact.method === "Email" ? 
+                                ("mailto:" + contact.info) : 
+                                contact.method === "Phone" ?
+                                ("tel:" + contact.info) :
+                                contact.info
+                            }
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            {
+                                contact.method === "Phone" ? 
+                                contact.readable :
+                                removeLinkPrefix(contact.info)
+                            }
+                        </a>
+                    </p>
                 ))
             }
         </div>
-    </section>
     );
 }
